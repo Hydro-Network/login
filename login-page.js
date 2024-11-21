91884e622635eaf0f45f58a66ebd83e25e9dfcd6
Hydro-Network <w1th3rxs@outlook.com>
2024-11-21 13:16:33 -0600

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -9,7 +9,6 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
-        location.reload();
       
 (url);
 
